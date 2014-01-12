b2db11a1e77610a9697a08fa7020a06822075a51
hjacobs <henning@jacobs1.de>
2014-01-12 19:48:34 +0100

    Working code to update price information from onvista.de and write it to GnuCash file

--- update-quotes.py
+++ update-quotes.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import datetime
+import logging
 import requests
 
 from bs4 import BeautifulSoup
-from gnucash import Session
+from gnucash import Session, GncNumeric
 from gnucash_patch import GncPrice
 
 
@@ -13,22 +15,58 @@ def get_quote_onvista_bond(isin):
     url = 'http://www.onvista.de/anleihen/snapshot.html?ISIN={}'.format(isin)
     r = requests.get(url)
     soup = BeautifulSoup(r.text)
-    price = soup.select('.INHALT #KURSINFORMATIONEN ~ .t span:nth-of-type(2)')[0].contents
-    print price
+    price = soup.select('.INHALT #KURSINFORMATIONEN ~ .t span:nth-of-type(2)')[0].get_text()
+    currency = 'EUR'
+    logging.info('Got quote for %s: %s%%', isin, price)
+    return GncNumeric(int(price.replace(',', '')), 100 * 100), currency
 
 
 def get_quote_onvista_stock(isin):
     url = 'http://www.onvista.de/suche/?searchValue={}'.format(isin)
     r = requests.get(url)
     soup = BeautifulSoup(r.text)
-    price = soup.select('.INHALT ul.KURSDATEN li:nth-of-type(1) span')[0].contents
-    print price
+    spans = soup.select('.INHALT ul.KURSDATEN li:nth-of-type(1) span')
+    price = spans[0].get_text()
+    currency = str(spans[1].get_text())
+    logging.info('Got quote for %s: %s %s', isin, price, currency)
+    return GncNumeric(int(price.replace(',', '')), 1000), currency
+
+
+def update_quote(commodity, book):
+    fullname = commodity.get_fullname()
+    mnemonic = commodity.get_mnemonic()
+    isin = commodity.get_cusip()
+    if len(isin) != 12:
+        return
+    logging.info('Processing %s (%s, %s)..', fullname, mnemonic, isin)
+    value, currency = None, None
+    try:
+        if name == 'BOND':
+            value, currency = get_quote_onvista_bond(isin)
+        else:
+            value, currency = get_quote_onvista_stock(isin)
+    except:
+        logging.exception('Failed to get quote for %s', isin)
+    if value and currency:
+        table = book.get_table()
+        gnc_currency = table.lookup('ISO4217', currency)
+        p = GncPrice(book)
+        p.set_time(datetime.datetime.now())
+        p.set_commodity(commodity)
+        p.set_currency(gnc_currency)
+        p.set_value(value)
+        book.get_price_db().add_price(p)
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('gnucash_file')
+parser.add_argument('--dry-run', action='store_true', help='Do not write anything, noop-mode')
 
 args = parser.parse_args()
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger('urllib3').setLevel(logging.WARN)
+
 s = Session(args.gnucash_file)
 try:
     book = s.book
@@ -36,18 +74,9 @@ try:
     for namespace in table.get_namespaces_list():
         name = namespace.get_name()
         if name != 'CURRENCY':
-            print name
             for commodity in namespace.get_commodity_list():
-                print commodity.get_mnemonic()
-                print commodity.get_fullname()
-                isin = commodity.get_cusip()
-                if len(isin) == 12:
-                    try:
-                        if name == 'BOND':
-                            get_quote_onvista_bond(isin)
-                        else:
-                            get_quote_onvista_stock(isin)
-                    except:
-                        print 'failed to get quote'
+                update_quote(commodity, book)
+    if not args.dry_run:
+        s.save()
 finally:
     s.end()
